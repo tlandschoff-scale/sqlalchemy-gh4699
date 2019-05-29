126b3cbf34bad08a8e465424a0901ba0b8c18a3b
Torsten Landschoff <torsten.landschoff@scale.eu>
2019-05-29 14:39:25 +0200

    Converted to classical mapping.

--- mixin_overrides.py
+++ mixin_overrides.py
@@ -1,67 +1,86 @@
-import contextlib
+# coding: utf-8
 
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
-from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData, Table, \
+    create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker, mapper
 
 from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.orderinglist import ordering_list
 
 
 # --[ Schema and mapped classes ]------------------------------------------
 
-Base = declarative_base()
+metadata = MetaData()
+
+entry_table = Table(
+    "entry", metadata,
+    Column("id", Integer, primary_key=True),
+    Column("entry_type", String),
+    Column("name", String),
+)
+
+file_table = Table(
+    "file", metadata,
+    Column("id", ForeignKey("entry.id"), primary_key=True),
+    Column("content", String),
+)
+
+directory_entry_table = Table(
+    "directory_entry", metadata,
+    Column("dir_id", Integer, ForeignKey("entry.id"), primary_key=True),
+    Column("entry_id", Integer, ForeignKey("entry.id"), primary_key=True),
+)
 
 
-class EntryCommon(Base):
-    __tablename__ = "entry"
-    id = Column(Integer, primary_key=True)
-    entry_type = Column(String)
-    name = Column(String)
-    __mapper_args__ = {'polymorphic_on': entry_type}
+class EntryCommon(object):
+    def __init__(self, name):
+        self.name = name
 
     def __repr__(self):
         return "<{0}: {1} ({2})>".format(self.__class__.__name__, self.name, self.id)
 
 
 class File(EntryCommon):
-    __tablename__ = "file"
-    id = Column(Integer, ForeignKey(EntryCommon.id), primary_key=True)
-    content = Column(String)
-    __mapper_args__ = {'polymorphic_identity': 'file'}
+    def __init__(self, name, content):
+        super(File, self).__init__(name=name)
+        self.content = content
 
 
 class Directory(EntryCommon):
-    __tablename__ = "directory"
-    id = Column(Integer, ForeignKey(EntryCommon.id), primary_key=True)
     entries = association_proxy("directory_entries", "entry")
-    __mapper_args__ = {'polymorphic_identity': 'directory'}
+
+    def __init__(self, name, entries=()):
+        super(Directory, self).__init__(name=name)
+        self.entries = entries
 
 
-class DirectoryEntry(Base):
-    __tablename__ = "directory_entry"
-    dir_id = Column(Integer, ForeignKey("directory.id"), primary_key=True)
-    entry_id = Column(Integer, ForeignKey("entry.id"), primary_key=True)
+class DirectoryEntry(object):
 
-    directory = relationship(Directory,
-        primaryjoin=Directory.id == dir_id,
-        backref=backref("directory_entries",
-                        cascade='all,delete-orphan')
-    )
-
-    entry = relationship(EntryCommon)
-
-    def __init__(self, entry, **kwargs):
-        super(DirectoryEntry, self).__init__(entry=entry, **kwargs)
+    def __init__(self, entry):
+        super(DirectoryEntry, self).__init__()
+        self.entry = entry
 
     def __repr__(self):
         return "<DirectoryEntry @ {0:x}>".format(id(self))
 
 
+mapper(EntryCommon, entry_table, polymorphic_on=entry_table.c.entry_type)
+mapper(File, file_table, inherits=EntryCommon, polymorphic_identity="file")
+mapper(Directory, local_table=None, inherits=EntryCommon, polymorphic_identity="directory")
+mapper(DirectoryEntry, directory_entry_table, properties={
+    "directory": relationship(
+        Directory,
+        primaryjoin=directory_entry_table.c.dir_id == entry_table.c.id,
+        backref=backref("directory_entries", cascade='all,delete-orphan'),
+    ),
+    "entry": relationship(EntryCommon,
+                          foreign_keys=[directory_entry_table.c.entry_id]),
+})
+
+
 # --[ Preparation ]------------------------------------------
 
-engine = create_engine("sqlite:///", echo=False)
-Base.metadata.create_all(engine)
+engine = create_engine("sqlite:///", echo=True)
+metadata.create_all(engine)
 
 session = sessionmaker(engine)()
 
